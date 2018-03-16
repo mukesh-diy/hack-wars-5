0cb45d8b80c6e18f6db5e26724f7d915d7b78aef
Mukesh <mukesh.kumar@hpe.com>
2018-03-15 23:48:55 -0500

    no message

--- home.js
+++ home.js
@@ -20,7 +20,7 @@ restService.get("/home", function(req, res) {
 });
 
 restService.post("/assist", function(req, res) {
-  var req_result = req.result;
+  var req_result = req.body.result;
   var req_action = req_result.action;
   if(req_action=='select.seat') {
     if(req_result.parameters.preference=='window') {
