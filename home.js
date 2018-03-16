c074e0440e7561d67dc53b91270cd8e336cfde2b
Mukesh <mukesh.kumar@hpe.com>
2018-03-15 21:39:12 -0500

    Initial commit.

---
+++ home.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.get("/home", function(req, res) {
+  return "Welcome to hack-wars application home page";
+});
+
+restService.post("/assist", function(req, res) {
+  var req_txt = req.body;
+  return res.json({
+    res_txt: req_txt,
+    displayText: res_txt,
+    source: "Hello There!"
+  });
+});
+
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server is up and listening");
+});
